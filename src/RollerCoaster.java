062c2c46e4708247ce2b8ea655151685efe9f129
Aldebaran <elton.alde@gmail.com>
2021-12-02 12:52:26 -0300

    Roller coaster class

---
+++ src/RollerCoaster.java
@@ -0,0 +1,90 @@
+import java.text.MessageFormat;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class RollerCoaster 
+{
+    private int passengerNumber;
+    private int cartNumber;
+    private int cartCapacity;
+
+    //Control
+    private ReentrantLock loadLock;
+    private ReentrantLock unloadLock;
+    private Condition load;
+    private Condition unload;
+	private Condition full;
+    private Condition empty;
+
+    private int queue = 0;
+
+    private int[] passengerInCart;
+	private boolean[] availableCart;
+
+    private int nextCartToLoad = 0;
+	private int nextCartToUnload = 0;
+
+    private int cartCont;
+	private int passengerCont;
+
+    //Other
+    private long waitTime;
+	private long dayBgn;
+	private long dayEnd;
+	private long dayDuration;
+
+	private int[] rides;
+
+    public RollerCoaster
+    (
+        int passengerNumber,
+        int cartNumber,
+        int cartCapacity
+    ){
+        this.passengerNumber = passengerNumber;
+        this.cartNumber      = cartNumber;
+        this.cartCapacity    = cartCapacity;
+
+        this.loadLock = new ReentrantLock();
+        this.load     = this.loadLock.newCondition();
+        this.full     = this.loadLock.newCondition();
+
+        this.unloadLock = new ReentrantLock();
+        this.unload     = this.unloadLock.newCondition();
+        this.empty      = this.unloadLock.newCondition();
+
+        this.passengerInCart = new int[cartCapacity];
+        this.availableCart = new boolean[cartNumber];
+
+        this.passengerCont  = passengerNumber;
+        this.cartCont       = cartNumber; 
+    }
+
+    private void CreateCarts()
+    {
+        int i;
+        for(i = 0; i > cartNumber; i++)
+        {
+            Thread cart = new Thread(new Cart(this, i));
+            System.out.println(MessageFormat.format("cart {0} created", i));
+            cart.start();
+        }
+    }
+
+    public boolean Load(int id)
+    {
+        return true;
+    }
+
+    public void Unload(int id)
+    {
+
+    }
+
+
+
+
+
+
+    
+}
