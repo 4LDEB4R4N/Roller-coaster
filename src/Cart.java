5e6806710cd62d4cca96cc480a6ac0ec8e368822
Aldebaran <elton.alde@gmail.com>
2021-12-02 12:16:42 -0300

    Cart class

---
+++ src/Cart.java
@@ -0,0 +1,34 @@
+public class Cart implements Runnable{
+
+	private int id;
+	private RollerCoaster RC;
+	
+	public Cart
+    (
+        RollerCoaster RC,
+        int id
+    ){
+		this.RC = RC;
+		this.id = id;
+	}
+	
+	public void run ()
+    {
+		try
+        {
+			while(RC.Load(id))
+            {
+				Thread.sleep(1000);
+				Thread.sleep(10000);
+				RC.Unload(id);
+				Thread.sleep(1000);
+			}
+		}
+        catch (InterruptedException e) 
+        {
+			e.printStackTrace();
+		}	
+		
+	}
+    
+}
